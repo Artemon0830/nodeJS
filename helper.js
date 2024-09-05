3fbbdd49caefdf518360be6d9dcbb525185b0b18
Artemon0830 <164068609+Artemon0830@users.noreply.github.com>
2024-09-05 17:12:02 +0200

    hw1

--- helper.js
+++
@@ -1,9 +0,0 @@
-// function saveHello(){
-//     console.log('Hello from helper.js');
-//     console.log('dirname',__dirname);
-//     console.log('filename',__filename);
-//     console.log('process.cwd',process.cwd());
-// }
-// module.exports={
-//     saveHello
-// }
