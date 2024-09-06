3235e9f593734f8218d87b113ad4bdc43fdeb985
Artemon0830 <164068609+Artemon0830@users.noreply.github.com>
2024-09-07 00:53:56 +0200

    hw1 add new position info directory and file

--- app.js
+++ app.js
@@ -10,25 +10,26 @@ const foo= async ()=> {
         console.log(folder)
         await fsPromises.mkdir(folder, {recursive: true})
         //створення файлів
-        for(let j=1;j<=5;j++){
-        const file = path.join(folder, `file${j}.txt`)
-        console.log(file)}
-        await fsPromises.writeFile(file, 'Hello world\n')
-        await fsPromises.appendFile(file, 'March 2024')
-        //прочитання файлу
-        const data = await fsPromises.readFile(file, 'utf-8')
-        console.log(data)
-        //інформація про папку
-        const statDirectory1 = await fsPromises.stat(baseFolder)
-        const statDirectory = await fsPromises.stat(folder)
-        console.log('info for directory baseFolder', statDirectory1.isDirectory())
-        console.log('info for directory folder', statDirectory.isDirectory())
-        //iнформація про файл
-        const statFile = await fsPromises.stat(file)
-        console.log('info for file',statFile.isFile())
-    
-    }
+        for(let j=1;j<=5;j++) {
+            const file = path.join(folder, `file${j}.txt`)
+            console.log(file)
+            await fsPromises.writeFile(file, 'Hello world\n')
+            await fsPromises.appendFile(file, 'March 2024')
+            //прочитання файлу
+            const data = await fsPromises.readFile(file, 'utf-8')
+            console.log(data)
+        }
 
+    }
+    //інформація про папку
+    const statDirectory1 = await fsPromises.stat(baseFolder)
+    const statDirectory = await fsPromises.stat(path.join(baseFolder, 'folder1'))
+    console.log('info for directory baseFolder', statDirectory1.isDirectory())
+    console.log('info for directory folder', statDirectory.isDirectory())
+    //iнформація про файл
+   const filePath = path.join(baseFolder, 'folder1', 'file1.txt');
+   const statFile = await fsPromises.stat(filePath)
+    console.log('info for file', statFile.isFile())
 
 
 }
